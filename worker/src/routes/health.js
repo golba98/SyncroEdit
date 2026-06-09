570032776febe7309edb1bec27883557ecdc5b2d
golba98 <jordanvorster404@gmail.com>
2026-06-10 01:41:40 +0200

    chore: Remove broken Cloudflare backend setup for clean redo

--- worker/src/routes/health.js
+++
@@ -1,8 +0,0 @@
-import { successResponse } from '../utils/responses.js';
-
-export function handleHealth(c) {
-  return successResponse(c, {
-    service: 'syncroedit-worker',
-    runtime: 'cloudflare-workers',
-  });
-}
